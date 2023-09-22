fc5c1c099a78b061d5a2a21c414d16798558f7b0
shiponirfan <imshiponirfan@gmail.com>
2023-09-23 02:07:17 +0600

    Credit Hour Remaining Function Added

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,29 @@ import Header from "./components/Header/Header";
 
 function App() {
   const [courseName, setCourseName] = useState([]);
+  const [creditHour, setCreditHour] = useState(0);
+  const [creditRemaining, setCreditRemaining] = useState(20);
   const handleCourseName = (name) => {
     const findName = courseName.find((nameFind) => nameFind.id === name.id);
+    let creditHour = name.credit;
     if (findName) {
-      alert("Name Already Declarer");
+      return alert("Name Already Declarer");
     } else {
-      setCourseName([...courseName, name]);
+      courseName.forEach((hour) => {
+        creditHour = creditHour + hour.credit;
+      });
+      if(creditHour > 20){
+        return alert("Credit Limit Is Over");
+      }else{
+        const creditHourRemaining = 20 - creditHour;
+        if(creditHourRemaining < 0){
+          return alert("Credit Limit Is End");
+        }else{
+          setCreditRemaining(creditHourRemaining);
+          setCreditHour(creditHour);
+          setCourseName([...courseName, name]);
+        }
+      }
     }
   };
   return (
@@ -20,7 +37,7 @@ function App() {
         <Header></Header>
         <div className="md:flex justify-between gap-6">
           <Courses handleCourseName={handleCourseName}></Courses>
-          <CreditHour courseName={courseName}></CreditHour>
+          <CreditHour creditRemaining={creditRemaining} creditHour={creditHour} courseName={courseName}></CreditHour>
         </div>
       </div>
     </>
--- src/components/CreditHour/CreditHour.jsx
+++ src/components/CreditHour/CreditHour.jsx
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types";
-const CreditHour = ({courseName}) => {
+const CreditHour = ({courseName, creditHour, creditRemaining}) => {
   return (
     <div className="w-1/4">
       <div className="card bg-white p-6">
-        <h2 className="text-lg font-bold text-[#2F80ED] pb-4 border-b-2">Credit Hour Remaining 7 hr</h2>
+        <h2 className="text-lg font-bold text-[#2F80ED] pb-4 border-b-2">Credit Hour Remaining {creditRemaining} hr</h2>
         <div className="pt-4 pb-6 border-b-2">
           <h2 className="text-xl font-bold">Course Name</h2>
           <ul className="pt-6 list-decimal list-inside">
@@ -12,13 +12,15 @@ const CreditHour = ({courseName}) => {
             }
           </ul>
         </div>
-        <h3 className="text-base font-medium text-[#1C1B1BCC] py-4 border-b-2">Total Credit Hour : 13</h3>
+        <h3 className="text-base font-medium text-[#1C1B1BCC] py-4 border-b-2">Total Credit Hour : {creditHour}</h3>
         <h3 className="text-base font-semibold text-[#1C1B1BCC] pt-4">Total Price : 48000 USD</h3>
       </div>
     </div>
   );
 };
 CreditHour.propTypes = {
-    courseName: PropTypes.array.isRequired
+    courseName: PropTypes.array.isRequired,
+    creditHour: PropTypes.number.isRequired,
+    creditRemaining: PropTypes.number.isRequired,
 }
 export default CreditHour;
